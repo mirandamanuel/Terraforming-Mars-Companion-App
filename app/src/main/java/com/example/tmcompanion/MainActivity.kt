e3352a7ccf3cd34944da2de917ab05dae8d47580
mirandamanuel <49087932+mirandamanuel@users.noreply.github.com>
2023-11-14 13:16:41 -0800

    Adjust layout sizing and positioning

--- app/src/main/java/com/example/tmcompanion/MainActivity.kt
+++ app/src/main/java/com/example/tmcompanion/MainActivity.kt
@@ -1,32 +1,35 @@
 package com.example.tmcompanion
 
-import android.graphics.drawable.Drawable
-import android.media.Image
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.graphics.Color as RGBColor
 import androidx.compose.ui.unit.dp
 import com.example.tmcompanion.ui.theme.TerraformingMarsCompanionAppTheme
+import androidx.compose.ui.graphics.Color as RGBColor
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,46 +46,92 @@ class MainActivity : ComponentActivity() {
                     .background(RGBColor(101, 20, 0))
                     .padding(16.dp)
                     .background(RGBColor.LightGray)
-                    .fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    .fillMaxSize() ,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround
                 ) {
-                    Row(modifier = Modifier) {
+                    //Row 1
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.LightGray),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
                         //Credits
-                        Column(modifier = Modifier) {
+                        Column(
+                            modifier = Modifier,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceAround
+                        ) {
                             ResourceTile(name = "Credits",
                                 icon = ImageVector.vectorResource(R.drawable.credit_icon))
                             ProductionRow(resource = "Credits", currentVal = creditsProd)
                         }
                         //Steel
-                        Column(modifier = Modifier) {
+                        Column(
+                            modifier = Modifier,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceAround
+                        ) {
                             ResourceTile(name = "Steel",
                                 icon = ImageVector.vectorResource(R.drawable.steel_icon))
                             ProductionRow(resource = "Steel", currentVal = steelProd)
                         }
                     }
-                    Row(modifier = Modifier) {
+                    //Row 2
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.LightGray),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
                         //Titanium
-                        Column(modifier = Modifier) {
+                        Column(
+                            modifier = Modifier,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceAround
+                        ) {
                             ResourceTile(name = "Titanium",
                                 icon = ImageVector.vectorResource(R.drawable.titanium_icon))
                             ProductionRow(resource = "Titanium", currentVal = titaniumProd)
                         }
                         //Plants
-                        Column(modifier = Modifier) {
+                        Column(
+                            modifier = Modifier,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceAround
+                        ) {
                             ResourceTile(name = "Plants",
                                 icon = ImageVector.vectorResource(R.drawable.plants_icon))
                             ProductionRow(resource = "Plants", currentVal = plantsProd)
                         }
                     }
-                    Row(modifier = Modifier) {
+                    //Row 3
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.LightGray),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
                         //Energy
-                        Column(modifier = Modifier) {
+                        Column(
+                            modifier = Modifier,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceAround
+                        ) {
                             ResourceTile(name = "Energy",
                                 icon = ImageVector.vectorResource(R.drawable.energy_icon))
                             ProductionRow(resource = "Energy", currentVal = energyProd)
                         }
                         //Heat
-                        Column(modifier = Modifier) {
+                        Column(
+                            modifier = Modifier,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceAround
+                        ) {
                             ResourceTile(name = "Heat",
                                 icon = ImageVector.vectorResource(R.drawable.heat_icon))
                             ProductionRow(resource = "Heat", currentVal = heatProd)
@@ -99,7 +148,9 @@ class MainActivity : ComponentActivity() {
 fun ResourceTile(name: String, icon: ImageVector){
     Icon(imageVector = icon ,
         contentDescription = name ,
-        modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 0.dp),
+        modifier = Modifier
+            .padding(16.dp, 16.dp, 16.dp, 0.dp)
+            .size(150.dp) ,
         tint = Color.Unspecified)
 }
 @Composable
@@ -111,4 +162,12 @@ fun ProductionRow(resource: String, currentVal: Int){
         Icon(imageVector = Icons.Default.ArrowForward ,
             contentDescription = null)
     }
+}
+
+fun getScreenWidth(): Int {
+    return Resources.getSystem().displayMetrics.widthPixels
+}
+
+fun getScreenHeight(): Int {
+    return Resources.getSystem().displayMetrics.heightPixels
 }
